1619749f833faf57e942240b671b1f4f64c1879b
Kenneth Ford <kford55@users.noreply.github.com>
2015-03-01 23:00:29 -0500

    Create upload_file.php
    Handles the upload of the maze, and then instantiates and runs the Maze object.

---
+++ upload_file.php
@@ -0,0 +1,33 @@
+<?php
+include 'upload_OOP.php';
+ini_set("display_errors", 1);
+$temp = explode(".", $_FILES["file"]["name"]);
+$extension = end($temp);
+
+
+$mazeFile = "../UPLOADS/".$_FILES["file"]["name"];
+  if ($_FILES["file"]["error"] > 0) {
+    echo "Return Code: " . $_FILES["file"]["error"] . "<br>";
+  } else {
+    echo "Upload: " . $_FILES["file"]["name"] . "<br>";
+    echo "Type: " . $_FILES["file"]["type"] . "<br>";
+    echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
+    echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br>";
+    if (file_exists("upload/" . $_FILES["file"]["name"])) {
+      echo $_FILES["file"]["name"] . " already exists. ";
+    } else {
+      if(move_uploaded_file($_FILES["file"]["tmp_name"],
+      $mazeFile)){
+      echo "Stored in: " . $mazeFile;}
+	  else{ echo "Could not move";}
+    }
+  }
+
+$mazeToRun = new Maze($mazeFile);
+$mazeToRun->display();
+if($mazeToRun->move($mazeToRun->start["x"],$mazeToRun->start["y"])){
+	echo "solved";
+	$mazeToRun->display();
+	}
+
+?>
